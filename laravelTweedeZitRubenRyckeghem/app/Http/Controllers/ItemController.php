6a716ccc5bb9d68ce02f6a8bffecec2e67e30d30
Ruben Ryckeghem <ruben.ryckeghem@student.ehb.be>
2019-07-27 16:20:05 +0200

    MVC item/itemcontroller met dummydata

--- laravelTweedeZitRubenRyckeghem/app/Http/Controllers/ItemController.php
+++ laravelTweedeZitRubenRyckeghem/app/Http/Controllers/ItemController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function getIndex(){
-        return view('welcome');
+        $item = new Item();
+        $items = $item->getItems();
+
+        return view('welcome',['items'=>$items]);
     }
 }
--- laravelTweedeZitRubenRyckeghem/app/Item.php
+++ laravelTweedeZitRubenRyckeghem/app/Item.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    public function getItems(){
+
+    return $this->createDummyData();
+
+    }
+
+    public function createDummyData(){
+        $items = [
+            ['id'=>1,'title' => 'Boardgames', 'content' => 'Leuke boardgames enzo'],
+            ['id'=>2,'title' => 'Gitaar', 'content' => 'Mooie gitaar van Gibson'],
+            ['id'=>3,'title' => 'Voetbal Nike', 'content' => 'Rode voetbal van Nike']
+        ];
+        return $items;
+    }
 }
