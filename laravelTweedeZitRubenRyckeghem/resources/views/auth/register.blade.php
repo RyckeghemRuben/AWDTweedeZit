7772b2a1a6fe4fbdafd4ca571ba5eafc464863e0
Ruben Ryckeghem <ruben.ryckeghem@student.ehb.be>
2019-07-31 11:18:39 +0200

    kleine aanpassing registerpagina
    Create van acc? == inschrijven voor ehackb

--- laravelTweedeZitRubenRyckeghem/resources/views/auth/register.blade.php
+++ laravelTweedeZitRubenRyckeghem/resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }} </div>
+                <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -59,7 +59,13 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right">{{ __('') }}</label>
 
+                            <div class="col-md-6">
+                                <label><i>Het aanmaken van een account schrijft je automatisch in voor het evenement.</i></label>
+                            </div>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
