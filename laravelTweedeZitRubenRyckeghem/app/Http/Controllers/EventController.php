65ede0d8fb5afb3bb250f30c1fb4e296f5f83265
Ruben Ryckeghem <ruben.ryckeghem@student.ehb.be>
2019-08-01 11:22:58 +0200

    probleem inschrijven opgelost
    Nu kan men zich per event inschrijven, anders kon men zich maar in 1 event inschrijven

--- laravelTweedeZitRubenRyckeghem/app/Http/Controllers/EventController.php
+++ laravelTweedeZitRubenRyckeghem/app/Http/Controllers/EventController.php
@@ -6,13 +6,17 @@ use App\Deelnemer;
 use Illuminate\Http\Request;
 use App\Event;
 use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class EventController extends Controller
 {
  public function getIndex(){
         $events = Event::orderBy('created_at','desc')->with('deelnemers')->get();
-        $deelnemer = Deelnemer::where('user_id', auth()->user()->id)->first();
-        return view('other.event',['events'=>$events, 'deelnemer'=>$deelnemer]);
+        $deelnemers = Deelnemer::all();
+        //haal enkel de values op van event_id van de deelnemer (met ->pluck())
+        $deelnemer = DB::table('deelnemers')->select('event_id')->where('user_id', '=', Auth::id())->pluck('event_id');
+        return view('other.event',['events'=>$events,'deelnemers'=>$deelnemers,'deelnemer'=>$deelnemer]);
  }
 
     public function getInschrijvenEventDeelnemer($id){
@@ -20,10 +24,10 @@ class EventController extends Controller
         $user = auth()->user();
         $deelnemer = new Deelnemer();
         $deelnemer->user_id = $user->id;
-
         $event->deelnemers()->save($deelnemer);
-
         return redirect()->back();
+
+
 
     }
 }
--- laravelTweedeZitRubenRyckeghem/resources/views/other/event.blade.php
+++ laravelTweedeZitRubenRyckeghem/resources/views/other/event.blade.php
@@ -4,23 +4,30 @@
 
     <div class="container">
         @foreach($events as $event)
-        <div class="card">
+        <div class="card" style="margin-bottom: 1em">
             <img src="{{$event->afbeelding}}" class="card-img-top" alt="overwatchToernooi">
             <div class="card-body">
                 <h5 class="card-title">{{$event->title}}</h5>
                 <p class="card-text">{{$event->content}}</p>
                 <footer class="blockquote-footer">{{$event->datumTijd}}</footer>
                 <br>
+                @if(Auth::check())
 
-                @if(Auth::check() && !$deelnemer)
+                    @if($deelnemer->contains($event['id']))
+                        <p>al ingeschreven</p>
+                    @else
+
                 <a href="{{route('inschrijvenEventDeelnemer',['id' => $event['id']])}}" class="btn btn-outline-dark">inschrijven</a>
                 <hr>
                     @endif
+                @else
+                    <p>Om je te kunnen inschrijven moet je inloggen</p>
+                @endif
+
                 <p>{{count($event->deelnemers)}}/10 deelnemers</p>
             </div>
         </div>
             @endforeach
-
     </div>
 
 
